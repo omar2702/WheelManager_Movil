fdecfa5fe18c8bcfeacf68c18c662ec3f6d36936
Maro <omaralonso27@gmail.com>
2021-05-04 17:08:42 -0500

    hot: Changes of navigation drawer

--- WheelManager/app/src/main/java/pe/edu/upc/wheelmanager/ui/gallery/GalleryViewModel.kt
+++ WheelManager/app/src/main/java/pe/edu/upc/wheelmanager/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is create Fragment"
+        value = "This is product Fragment"
     }
     val text: LiveData<String> = _text
 }
