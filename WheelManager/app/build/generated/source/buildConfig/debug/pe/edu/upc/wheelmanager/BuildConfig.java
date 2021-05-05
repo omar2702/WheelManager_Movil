533625a82119b6dfa9d3daceea5ce6965a84193a
Bengalaz <ffreddy8900@gmail.com>
2021-05-05 05:53:05 -0500

    Navigation_var implemented

---
+++ WheelManager/app/build/generated/source/buildConfig/debug/pe/edu/upc/wheelmanager/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package pe.edu.upc.wheelmanager;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "pe.edu.upc.wheelmanager";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
