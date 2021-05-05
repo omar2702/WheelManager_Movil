69abe44b6315987f0eb25875226e14333750d2d2
Maro <omaralonso27@gmail.com>
2021-05-04 19:18:28 -0500

    hot: Create layout ServiceFragment.kt in navigation.

--- WheelManager/app/src/main/java/pe/edu/upc/wheelmanager/ui/CreateService/CreateViewModel_CreateService.kt
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.wheelmanager.ui.CreateService
-
-import androidx.lifecycle.ViewModel
-
-class CreateViewModel_CreateService : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- WheelManager/app/src/main/java/pe/edu/upc/wheelmanager/ui/slideshow/ServiceFragment.kt
+++ WheelManager/app/src/main/java/pe/edu/upc/wheelmanager/ui/slideshow/ServiceFragment.kt
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import pe.edu.upc.wheelmanager.R
 
-class SlideshowFragment : Fragment() {
+class ServiceFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+    private lateinit var serviceViewModel: ServiceViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        serviceViewModel =
+            ViewModelProvider(this).get(ServiceViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_service, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+        serviceViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
